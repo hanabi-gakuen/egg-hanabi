0afbf3124e367053748421f51a218a7774db80c7
Illyrix <Illyrix@outlook.com>
2018-02-08 16:16:40 +0800

    init structure

---
+++ app/service/test.js
@@ -0,0 +1,14 @@
+const Service = require('egg').Service
+
+class TestService extends Service {
+  constructor(ctx) {
+    super(ctx)
+    this.config = this.app.config.test
+  }
+
+  async get(id) {
+    return { id, name: this.config.key }
+  }
+}
+
+module.exports = TestService
---
+++ test/fixtures/example/app/controller/home.js
@@ -0,0 +1,10 @@
+const Controller = require('egg').Controller
+
+class HomeController extends Controller {
+  async index() {
+    const data = await this.service.test.get(123)
+    this.ctx.body = data.name
+  }
+}
+
+module.exports = HomeController
